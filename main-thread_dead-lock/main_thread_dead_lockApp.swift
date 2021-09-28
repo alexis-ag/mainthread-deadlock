185cfaad20bd7042d13978ea096ccd26bc2f40fc
alexis <alexis@wildapricot.com>
2021-09-28 18:58:49 +0300

    sample

---
+++ main-thread_dead-lock/main_thread_dead_lockApp.swift
@@ -0,0 +1,17 @@
+//
+//  main_thread_dead_lockApp.swift
+//  main-thread_dead-lock
+//
+//  Created by Alexis Grigorev on 27.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct main_thread_dead_lockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
